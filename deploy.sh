2d6c46b07e3d1a6acc48d0d3cc2bc58e2e566ef4
benjaminangafua1997 <benjaminarkutl2017@gmail.com>
2022-11-12 07:51:59 +0000

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:benjaminangafua1997/silk-road-vue-composition-api.git main:gh-pages
+git push -f https://github.com/benjaminangafua1997/silk-road-vue-composition-api.git master:gh-pages
 
 cd -
