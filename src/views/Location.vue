5480cf039a0c8cbf0819574d0915a06619f635f5
benjaminangafua1997 <benjaminarkutl2017@gmail.com>
2022-12-04 22:42:44 +0000

    Updated property details

--- src/views/Location.vue
+++ src/views/Location.vue
@@ -16,18 +16,29 @@ import HeadTop from '../components/HeadTop.vue';
                 <div class="property-details"><RouterLink to="/post-a-property/detail">Property Details</RouterLink> </div>
                 <div class="contact-details"><RouterLink to="/post-a-property/contact-detail"> Contact Details</RouterLink> </div>
             </div>
-            <div class="select-category"><strong>Select Category</strong></div>
-            <div>
-                <hr>
-            </div>
-            <div class="category-list">
-                <div class="houses">House span</div>
-                <div class="apartments">Apartments</div>
-                <div class="office-space">Office Spaces</div>
-                <div class="storage-space">Storage Spaces</div>
-                <div class="rooms">Rooms</div>
-                <div class="land">Land</div>
-                <div class="commercial-properties">Commercial Properties</div>
+
+            <div class="select-county"><strong>Select county</strong></div> 
+
+            <div class="location-list">
+                <div class="county-list">
+                    <b>Counties</b>
+                    <div class="county">Bomi</div>
+                    <div class="county">Bong</div>
+                    <div class="county">Gbapolu</div>
+                    <div class="county">Grand Bassa</div>
+                    <div class="county">Grand Gedeh</div>
+                    <div class="county">Grand Kru</div>
+                    <div class="county">Grand Cape Mount</div>
+                    <div class="county">Lofa</div>
+                    <div class="county">Margibi</div>
+                    <div class="county">Maryland</div>
+                    <div class="county">Montserrado</div>
+                    <div class="county">Nimba</div>
+                    <div class="county">River Cess</div>
+                    <div class="county">River Gee</div>
+                    <div class="county">Sinoe</div>
+                </div>
+                <div class="district-list"></div>
             </div>
         </div>
         <div id="footer"><FooterDown /></div>
